192451aba2d8a81c3e8771a8378f84126470eae9
soartec-lab <info@soartec-lab.work>
2024-12-30 07:18:09 +0000

    fix: update db schema

--- packages/backend/db/migrations/20241230061647-createTablePets/migration.sql
+++ packages/backend/db/migrations/20241230061647-createTablePets/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE `pets` (
-    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
