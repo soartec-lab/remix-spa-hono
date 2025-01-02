ca801e0484357cc97fdf70b7c45d955b75623217
soartec-lab <info@soartec-lab.work>
2025-01-02 01:47:31 +0000

    chore: add create sample code

--- packages/backend/src/handlers/createPets.ts
+++ packages/backend/src/handlers/createPets.ts
@@ -3,9 +3,19 @@ import type { CreatePetsContext } from "../api.context";
 import { zValidator } from "../api.validator";
 import { createPetsBody } from "../api.zod";
 
+import { Pet } from "../models/pet";
+
 const factory = createFactory();
 
 export const createPetsHandlers = factory.createHandlers(
 	zValidator("json", createPetsBody),
-	async (c: CreatePetsContext) => {},
+	async (c: CreatePetsContext) => {
+		const pet = { name: "name" };
+
+		await Pet.create({
+			data: pet,
+		});
+
+		return c.json(pet);
+	},
 );
